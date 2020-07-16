a4eba91607b12b2638bd0342d410fe43ad367618
William Ho <william.ho3@ucalgary.ca>
2020-07-16 14:53:02 -0600

    Initial Commit

---
+++ Coffee Log/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffee Log
+//
+//  Created by William Ho on 2020-07-16.
+//  Copyright © 2020 William Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
